4af6a3a0cbe4c314f94e27467f1dbc26d0bf6f56
test <sadiq.hassan@gmail.com>
2022-11-22 13:16:44 +1100

    Assignment Khawaja

--- js/index.js
+++ js/index.js
@@ -19,6 +19,9 @@
       5. Add a countdown timer - when the time is up, end the quiz, display the score and highlight the correct answers
 *************************** */
 
+
+
+
 window.addEventListener('DOMContentLoaded', () => {
   const start = document.querySelector('#start');
   start.addEventListener('click', function (e) {
@@ -44,47 +47,129 @@ window.addEventListener('DOMContentLoaded', () => {
       o: ['Sydney', 'Canberra', 'Melbourne', 'Perth'],
       a: 1,
     },
+    {
+      q: 'What is the capital of United Kingdom',
+      o: ['London', 'Scotland', 'Ireland', 'Lisbon'],
+      a: 0,
+    },
+    {
+      q: 'What is the capital of India',
+      o: ['Bombay', 'New Delhi', 'Nagpur', 'Gujrat'],
+      a: 1,
+    }
+
   ];
 
   // function to Display the quiz questions and answers from the object
   const displayQuiz = () => {
     const quizWrap = document.querySelector('#quizWrap');
+
     let quizDisplay = '';
     quizArray.map((quizItem, index) => {
       quizDisplay += `<ul class="list-group">
-                   Q - ${quizItem.q}
+                   Q:${index+1} - ${quizItem.q} 
                     <li class="list-group-item mt-2" id="li_${index}_0"><input type="radio" name="radio${index}" id="radio_${index}_0"> ${quizItem.o[0]}</li>
                     <li class="list-group-item" id="li_${index}_1"><input type="radio" name="radio${index}" id="radio_${index}_1"> ${quizItem.o[1]}</li>
                     <li class="list-group-item"  id="li_${index}_2"><input type="radio" name="radio${index}" id="radio_${index}_2"> ${quizItem.o[2]}</li>
                     <li class="list-group-item"  id="li_${index}_3"><input type="radio" name="radio${index}" id="radio_${index}_3"> ${quizItem.o[3]}</li>
+                    <li class="list-group-item"  id="li_${index}_4"><input type="radio" name="radio${index}" id="radio_${index}_4"> ${quizItem.o[4]}</li>
+                    <li class="list-group-item"  id="li_${index}_5"><input type="radio" name="radio${index}" id="radio_${index}_5"> ${quizItem.o[5]}</li>
+
+
                     </ul>
                     <div>&nbsp;</div>`;
       quizWrap.innerHTML = quizDisplay;
+
     });
   };
+  //Event Listener for the submit button
+
+  const btn = document.getElementById('btnSubmit');
+  btn.addEventListener('click', function(event) {
+    const totalScore = calculateScore(this);
+    
+    document.getElementById('output').innerHTML = "Your Total Score is " +  totalScore;
+
+  });
+
+
 
   // Calculate the score
   const calculateScore = () => {
     let score = 0;
+
     quizArray.map((quizItem, index) => {
-      for (let i = 0; i < 4; i++) {
+      for (let i = 0; i < 6; i++) {
         //highlight the li if it is the correct answer
         let li = `li_${index}_${i}`;
         let r = `radio_${index}_${i}`;
         liElement = document.querySelector('#' + li);
         radioElement = document.querySelector('#' + r);
-
-        if (quizItem.a == i) {
-          //change background color of li element here
-        }
-
         if (radioElement.checked) {
-          // code for task 1 goes here
+          if (quizItem.a == i) {
+            score++;
+            liElement.style.backgroundColor = '#00ff00';
+          } else {
+            liElement.style.backgroundColor = '#800000';
+          }
+        } else if (quizItem.a == i) {
+          liElement.style.backgroundColor = '#FBCEB1';
+
+          	
         }
+
+
+        
       }
     });
+    return score;
   };
 
+  
   // call the displayQuiz function
   displayQuiz();
+
+  // reset quiz 
+
+  let fullReset = document.getElementById('btnReset');
+
+    fullReset.addEventListener('click', function(e) {
+      location.reload();
+    }, false);
+
+
+// Timer 
+
+let total_seconds = 30 * 1;
+let c_minutes = parseInt(total_seconds / 60);
+let c_seconds = parseInt(total_seconds % 60);
+let timer;
+
+function CheckTime() {
+  document.getElementById("time").innerHTML =  c_minutes + ' minutes ' + c_seconds + ' seconds ';
+
+  if (total_seconds <= 0) {
+    stopNDisable();
+  } else {
+    total_seconds = total_seconds - 1;
+    c_minutes = parseInt(total_seconds / 60);
+    c_seconds = parseInt(total_seconds % 60);
+    timer = setTimeout(CheckTime, 1000);
+  }
+}
+
+timer = setTimeout(CheckTime, 1000);
+function stopNDisable(){
+
+  for (let i = 0; i < quizArray.length; ++i) {
+    quizArray[i].disabled = true;
+  }
+
+}
+
+
+
+//timer emds 
+
+
 });
